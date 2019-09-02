dce9010b84bba06709c2b65684b4fea1b264a69d
andikajayaw <andikajayawiguna@gmail.com>
2019-09-02 22:17:07 +0700

    add exercise-7.js

---
+++ exercise-7.js
@@ -0,0 +1,20 @@
+function hitungJumlahKata(kalimat) {
+    // you can only write your code here!
+    var hitung = 0
+    kalimat = kalimat + ' '
+
+    for (var i = 0; i < kalimat.length; i++) {
+        if (kalimat[i] === ' ') {
+            hitung = hitung + 1
+        }
+    }
+    return hitung
+
+}
+
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
