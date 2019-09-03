335b8c9a45bfd39ea5b81727a4cb4a6895480ef7
andikajayaw <andikajayawiguna@gmail.com>
2019-09-03 13:18:30 +0700

    add exercise-6.js

---
+++ exercise-6.js
@@ -0,0 +1,20 @@
+function angkaPalindrome(num) {
+    // you can only write your code here!
+    num++
+    while (true) {
+        cekPalindrome = num === Number(num.toString().split('').reverse().join(''))
+        console.log("Angka => " + num)
+        if (cekPalindrome === false) {
+            num++
+        } else {
+            return num
+        }
+    }
+}
+
+// TEST CASES
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001
