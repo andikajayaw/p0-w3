98f716c62c79d25b569f214f9b1b91431d77b6e3
andikajayaw <andikajayawiguna@gmail.com>
2019-09-05 15:27:30 +0700

    add exercise-15.js

---
+++ exercise-15.js
@@ -0,0 +1,29 @@
+function groupAnimals(animals) {
+    // you can only write your code here!
+    animals.reverse();
+    animals.sort();
+
+    var temp = []
+    var result = []
+
+    for (var i = 0; i < animals.length; i++) {
+        temp.push(animals[i])
+        if (animals[i + 1] !== undefined) {
+            if (animals[i][0] !== animals[i + 1][0]) {
+                result.push(temp)
+                temp = []
+            }
+        }
+    }
+    result.push(temp)
+    return result
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+
+
+
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
